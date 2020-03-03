7a8d1b7cd4c5c85e9c9b9f2d434b0b30335506ca
yael <yael.atletl@gmail.com>
2020-03-03 02:05:14 -0600

    Remove the failed IK characters

--- Models/Pfhor Trooper_Fighter/IK_Pfhor.gd
+++
@@ -1,9 +0,0 @@
-tool
-extends Spatial
-
-
-func _ready():
-	$AnimationPlayer.playback_process_mode = AnimationPlayer.ANIMATION_PROCESS_PHYSICS 
-
-func _process(delta):
-	$AnimationPlayer.play("IK_Play")
